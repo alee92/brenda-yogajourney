fe5900993275f7f07b478ee04e375e4425665f45
alee92 <mistahleejr@gmail.com>
2015-07-24 00:44:07 -0700

    Delete Everything - TEST

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
-<?php
-
-set_time_limit(0);
-ini_set('default_socket_timeout', 300);
-session_start();
-
-/*********** Instagram API Keys ************/
-
-define("clientID", '45ff7235ae5841268cd947158f664c23');
-define("clientSecret", '8bb0ed77337145eb8f47d1edbb365faa');
-define("redirectURI", 'https://brenda-yogajourney.herokuapp.com');
-define("imageDirectory", 'pics/');
-
-?>
-
-<!doctype html>
 
 <html>
 <body>
-	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?> &redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
+	<h1> Test </h1>
 </body>
 </html>
