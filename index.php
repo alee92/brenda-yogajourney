f9dbe45bef4e8827f368abca13594f307eb7ba01
alee92 <mistahleejr@gmail.com>
2015-07-24 00:11:01 -0700

    Add OAuth link

--- index.php
+++ index.php
@@ -8,7 +8,15 @@ session_start();
 
 define("clientID", '45ff7235ae5841268cd947158f664c23');
 define("clientSecret", '8bb0ed77337145eb8f47d1edbb365faa');
-define("redirectURL", 'https://brenda-yogajourney.herokuapp.com');
+define("redirectURI", 'https://brenda-yogajourney.herokuapp.com');
 define("imageDirectory", 'pics/');
 
 ?>
+
+<!doctype html>
+
+<html>
+<body>
+	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?> &redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
+</body>
+</html>
